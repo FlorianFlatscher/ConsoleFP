4dca1ce82a89b950d8f6f6539cb13e0f0e9aa1eb
greenappleFF <florian.flatscher@gmail.com>
2020-04-01 13:37:36 +0200

    Fix application

--- src/application/Application.java
+++ src/application/Application.java
@@ -1,5 +1,6 @@
 package application;
 
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
@@ -11,28 +12,31 @@ import javafx.stage.Stage;
 import java.io.File;
 
 public class Application extends javafx.application.Application {
-    public Label game = new Label("ajsdlöfjöalsdjflöaj");
 
     @Override
     public void start(Stage stage) throws Exception {
-        game.setStyle("-fx-font-family: 'monospaced';");
-        setup(stage);
-    }
-
-    private void setup(Stage stage) {
         VBox root = new VBox();
-        root.getChildren().add(game);
-    }
+        stage.setScene(new Scene(root, 600, 400));
 
-    private void menu(VBox root, Stage stage) {
-        MenuBar bar = new MenuBar();
-        Menu file = new Menu("File");
+        //Menu
         MenuItem openFile = new MenuItem("Open...");
         openFile.setOnAction(actionEvent -> {
             FileChooser chooser = new FileChooser();
             File file1 = chooser.showOpenDialog(stage);
         });
+        Menu fileMenu = new Menu("File", null, openFile);
+        MenuBar bar = new MenuBar(fileMenu);
+
+
+        //Content
+        Label game = new Label("test\n....\n----\n||||");
+        game.setStyle("-fx-font-family: 'monospaced';");
+
+        root.getChildren().addAll(game);
+
+        stage.show();
     }
+
 
     public static void main(String[] args) {
         launch(args);
