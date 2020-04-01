b0ce6e7a6b76c7f95a8529fd012fd4dd40c98dae
Christoph Kainz <ckainz11@users.noreply.github.com>
2020-04-01 13:09:35 +0200

    Add Application.java

---
+++ src/application/Application.java
@@ -0,0 +1,41 @@
+package application;
+
+import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+
+public class Application extends javafx.application.Application {
+    public Label game = new Label("ajsdlöfjöalsdjflöaj");
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        game.setStyle("-fx-font-family: 'monospaced';");
+        setup(stage);
+    }
+    private void setup(Stage stage){
+        VBox root = new VBox();
+        root.getChildren().add(game);
+
+    }
+    private void menu(VBox root, Stage stage){
+        MenuBar bar = new MenuBar();
+        Menu file = new Menu("File");
+        MenuItem openFile = new MenuItem("Open...");
+        openFile.setOnAction(actionEvent -> {
+            FileChooser chooser = new FileChooser();
+            File file1 = chooser.showOpenDialog(stage);
+        });
+
+
+
+    }
+    public static void main(String[] args){
+        launch(args);
+    }
+}
